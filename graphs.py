c950cfb52428d0c47456beb7557680904ce5cb05
ADI <adi@ADIs-MacBook-Pro.local>
2024-07-11 12:40:12 -0400

    added Pylint edits

--- graphs.py
+++ graphs.py
@@ -6,10 +6,22 @@ that when called generate a plot.
 # Importing the required libraries.
 import matplotlib.pyplot as plt
 import seaborn as sns
-def symptom_by_vax_manu(data_EU):
-    all_symptoms = data_EU.explode(["SYMPTOMS_LIST"])
+def symptom_by_vax_manu(data_eu):
+    """
+    This file is used for generating final graphs
+    for the analysis of the project. There are functions
+    that when called generate a plot.
+
+    **symptom_by_vax_manu** function generates a bar chart showing the top 
+    20 most frequent symptoms reported for the Pfizer vaccine in the EU data.
+    Parameters:
+    data_eu (pd.DataFrame): DataFrame containing the EU vaccination data.
+    Returns:
+    matplotlib.pyplot.Figure: The plot object representing the bar chart.
+    """
+# Importing the required libraries.
+    all_symptoms = data_eu.explode(["SYMPTOMS_LIST"])
     eu_exploded = all_symptoms.dropna(subset=['SYMPTOMS_LIST'])
-    #reaction_counts = eu_exploded[eu_exploded['SYMPTOMS_LIST'].isin(symptoms_list)]
     pfizer_symptoms = eu_exploded[eu_exploded['VAX_MANU'] == 'PFIZER']
     reaction_counts1 = pfizer_symptoms['SYMPTOMS_LIST'].value_counts()
     reaction_counts_df = reaction_counts1.reset_index()
@@ -17,7 +29,15 @@ def symptom_by_vax_manu(data_EU):
     top_20_sym_pfizer = reaction_counts_df.head(20)
     plt.figure(figsize=(12, 8))
     # Create the bar plot
-    sns.barplot(x='Count', y='Symptom', data=top_20_sym_pfizer.sort_values('Count', ascending=False), palette='viridis', hue='Symptom', dodge=False, legend=False)
+    sns.barplot(
+    x='Count',
+    y='Symptom',
+    data=top_20_sym_pfizer.sort_values('Count', ascending=False),
+    palette='viridis',
+    hue='Symptom',
+    dodge=False,
+    legend=False
+)
     plt.xlabel('Count')
     plt.ylabel('Symptom')
     plt.title('Top 20 Reported Symptoms of Pfizer in EU')
--- main.py
+++ main.py
@@ -1,52 +1,73 @@
-#Importing all the libraries
+"""
+This script processes EU Pfizer vaccine data, extracts symptoms, and generates
+a bar plot of the top 20 reported symptoms.
+"""
+import glob  # Standard import
+import re  # Standard import
 import pandas as pd
-import glob
-import matplotlib.pyplot as plt 
-import seaborn as sns
-import os
-import re
 import graphs as gp
 
 
-#"Merging all the EU data files"
-def clean(data_EU_pfizer):
-    def extract_symptoms(symptom_list):
+# Merging all the EU data files
+def clean(data_frame):
+    """
+    Cleans the data for EU Pfizer vaccine reports.
 
+    Parameters:
+    data_frame (pd.DataFrame): DataFrame containing EU Pfizer vaccine data.
+
+    Returns:
+    pd.DataFrame: Cleaned DataFrame.
+    """
+
+    def extract_symptoms(symptom_list):
+        """
+        Extracts symptoms from the symptom list.
+
+        Parameters:
+        symptom_list (str): String containing symptoms separated by <BR><BR> tags.
+
+        Returns:
+        list: List of symptoms.
+        """
         if isinstance(symptom_list, str):
-            # Split the reactions using the HTML break tag as the delimiter
             symptoms = re.split(r'<BR><BR>', symptom_list)
-            # Further clean and extract just the reaction name before the first parenthesis
             symptoms = [re.split(r'\(', symptom.strip())[0].strip() for symptom in symptoms]
             return symptoms
-        else:
-            return []
-    # Renaming the columns  
-    data_EU_pfizer.columns = ["EU_ID", "1", "2", "3", "4", "5", "AGE_GROUP", "6", "7", "SEX", "SYMPTOMS_LIST", "8", "9", "10"]
+        return []
+    # Renaming the columns
+    data_frame.columns = ["EU_ID", "1", "2", "3", "4", "5", "AGE_GROUP",
+                              "6", "7", "SEX", "SYMPTOMS_LIST", "8", "9", "10"]
     # Selection of specific columns
-    data_EU_pfizer = data_EU_pfizer[["EU_ID", "AGE_GROUP", "SEX", "SYMPTOMS_LIST"]]
+    data_frame = data_frame[["EU_ID", "AGE_GROUP", "SEX", "SYMPTOMS_LIST"]]
     # Applying the function to the symptom list column to extract the specific symptoms
-    data_EU_pfizer["SYMPTOMS_LIST"] = data_EU_pfizer["SYMPTOMS_LIST"].apply(extract_symptoms)
+    data_frame["SYMPTOMS_LIST"] = data_frame["SYMPTOMS_LIST"].apply(extract_symptoms)
     # Renaming the age group column
-    data_EU_pfizer["AGE_GROUP"] = data_EU_pfizer["AGE_GROUP"].astype(str).str.replace("Years", "").str.strip()
-     # Renaming the age group column
-    data_EU_pfizer["SEX"] = data_EU_pfizer["SEX"].astype(str).str.replace("emale", "").str.strip()
-    data_EU_pfizer["SEX"] = data_EU_pfizer["SEX"].astype(str).str.replace("ale", "").str.strip()
-    data_EU_pfizer["SEX"] = data_EU_pfizer["SEX"].astype(str).str.replace("Not Specified", "U").str.strip()
-     # Renaming the VAX_MANU column to Pfizer
-    data_EU_pfizer['VAX_MANU'] = "PFIZER"
-    return data_EU_pfizer
+    data_frame["AGE_GROUP"] = data_frame["AGE_GROUP"].astype(str).str.replace(
+        "Years", ""
+    ).str.strip()
+    # Renaming the sex column
+    data_frame["SEX"] = data_frame["SEX"].astype(str).str.replace("emale", "F").str.strip()
+    data_frame["SEX"] = data_frame["SEX"].astype(str).str.replace("ale", "M").str.strip()
+    data_frame["SEX"] = data_frame["SEX"].astype(str).str.replace(
+        "Not Specified", "U"
+    ).str.strip()
+    # Renaming the VAX_MANU column to Pfizer
+    data_frame['VAX_MANU'] = "PFIZER"
+    return data_frame
+
+
 if __name__ == '__main__':
-    data_EU_pfizer = pd.DataFrame()
+    data_eu_pfizer = pd.DataFrame()
     for file_name in glob.glob('EU Files/EU PFIZER' + '*.csv'):
-        x = pd.read_csv(file_name, low_memory = False)
-        data_EU_pfizer = pd.concat([data_EU_pfizer, x], axis = 0, ignore_index = True)      
-    data = clean(data_EU_pfizer)
-    data.to_csv("output_files/eu_output.csv")
+        x = pd.read_csv(file_name, low_memory=False)
+        data_eu_pfizer = pd.concat([data_eu_pfizer, x], axis=0, ignore_index=True)
+    clean_data= clean(data_eu_pfizer)
+    clean_data.to_csv("output_files/eu_output.csv")
 
-  # Creating a bar plot of the number of symptoms by Pfizer vaccine in EU
+    # Creating a bar plot of the number of symptoms by Pfizer vaccine in EU
+    eu_output = pd.read_csv("/Users/adi/Desktop/MS1/git/output_files/eu_output.csv")
+    plot = gp.symptom_by_vax_manu(eu_output)
 
-eu_output = pd.read_csv("/Users/adi/Desktop/MS1/git/output_files/eu_output.csv")
-plt = gp.symptom_by_vax_manu(eu_output)
-
-# Save the plot
-plt.savefig('/Users/adi/Desktop/MS1/git/output_files/symptoms_by_pfizer_barplot.png')
+    # Save the plot
+    plot.savefig('/Users/adi/Desktop/MS1/git/output_files/symptoms_by_pfizer_barplot.png')
